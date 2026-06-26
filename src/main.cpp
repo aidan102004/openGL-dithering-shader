a8b72f32cd4169bd02fcc5eebecd48f6475a5960
aidan102004 <aidan.abidi@gmail.com>
2026-06-26 17:24:27 +1000

    added cube vertices

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,47 @@
 #include <GLFW/glfw3.h>
 
 using namespace std; 
+float vertices[] = {
+    // Front face
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    // Back face
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    // Left face
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    // Right face
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+    // Top face
+    -0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    // Bottom face
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+};
 
+unsigned int indices[] = {
+     0,  1,  2,   2,  3,  0,  
+     4,  6,  5,   6,  4,  7,  
+     8,  9, 10,  10, 11,  8,  
+    12, 14, 13,  14, 12, 15,  
+    16, 17, 18,  18, 19, 16,  
+    20, 22, 21,  22, 20, 23,  
+};
 void framebuffer_size_callback(GLFWwindow* window, int w, int h);
 void processInput(GLFWwindow* window);
 int main() {
